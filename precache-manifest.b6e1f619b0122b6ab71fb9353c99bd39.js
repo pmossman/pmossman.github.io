3c307b0ba0cbd95d802fe1b3f92d10418591d740
Parker Mossman <parker.mossman@liveramp.com>
2019-09-27 19:12:17 -0700

    add unichord

---
+++ precache-manifest.b6e1f619b0122b6ab71fb9353c99bd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0988f496292fd687d811663396463f3",
+    "url": "/unichord/index.html"
+  },
+  {
+    "revision": "83e44e5949cec2e9222e",
+    "url": "/unichord/static/css/main.26cd466b.chunk.css"
+  },
+  {
+    "revision": "f58c464bdd43f3ecbc22",
+    "url": "/unichord/static/js/2.ff7242bd.chunk.js"
+  },
+  {
+    "revision": "83e44e5949cec2e9222e",
+    "url": "/unichord/static/js/main.70b8beb2.chunk.js"
+  },
+  {
+    "revision": "81a5af7517d9ac79dcb5",
+    "url": "/unichord/static/js/runtime~main.05d9f94e.js"
+  }
+]);
