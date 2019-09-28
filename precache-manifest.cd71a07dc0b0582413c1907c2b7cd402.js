31861d3f1a2c444d2c520f6dcee4ac987740622c
Parker Mossman <parker.mossman@liveramp.com>
2019-09-27 19:16:54 -0700

    try deploy

---
+++ precache-manifest.cd71a07dc0b0582413c1907c2b7cd402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71642b4b5f5c730e25eabdcfc61700bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67d81ac57543c4fa84f",
+    "url": "/static/css/main.26cd466b.chunk.css"
+  },
+  {
+    "revision": "f58c464bdd43f3ecbc22",
+    "url": "/static/js/2.ff7242bd.chunk.js"
+  },
+  {
+    "revision": "b67d81ac57543c4fa84f",
+    "url": "/static/js/main.4988c219.chunk.js"
+  },
+  {
+    "revision": "98a1fd7eb9b3f174e691",
+    "url": "/static/js/runtime~main.521595ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unichord/precache-manifest.b6e1f619b0122b6ab71fb9353c99bd39.js"
+  "/precache-manifest.cd71a07dc0b0582413c1907c2b7cd402.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unichord/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
